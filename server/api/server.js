0c54b8967ed282e31aa41b924dbaea3c93105e3d
mumblesrebel <johnnycotts@hotmail.com>
2025-04-15 16:28:02 +1000

    Fix CORS configuration in server.js

--- server/api/server.js
+++ server/api/server.js
@@ -28,10 +28,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    console.log('Request origin:', origin);
-    callback(null, true);
-  },
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
